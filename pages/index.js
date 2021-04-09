5fb4b2a0a2acbd7c24e20020219dd4b542f9c0e2
Ritik Nandanwar <nandanwarritik10@gmail.com>
2021-04-09 20:47:03 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Link from "next/link"
+export const getStaticProps = async () => {
+  const data = await fetch("https://www.balldontlie.io/api/v1/players");
+  const players = await data.json();
+  console.log(players);
+  return {
+    props: {
+      players,
+    },
+  };
+};
+export default function Home({ players }) {
+  return (
+    <div className="bg-blue-300 h-screen mx-auto overflow-scroll overflow-x-hidden">
+      <h1 className="text-blue-600 py-12">View all NBA players</h1>
+      <div className="w-9/12 mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+        {players.data.map((player) => (
+          <Link href={"/" +player.id} key={player.id}>
+          <a>
+            <div>
+            {" "}
+            <div class="max-w-sm rounded bg-blue-200 overflow-hidden shadow-lg py-6">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">
+                  {player.first_name} {player.last_name}
+                </div>
+              </div>
+              <h3 className="px-6 mb-3">
+                Team
+                <span className="ml-2 font-bold">{player.team.full_name}</span>
+              </h3>
+              <h3 className="px-6 mb-3">
+                City
+                <span className="ml-2 font-bold">{player.team.city}</span>
+              </h3>
+              <h3 className="px-6 mb-3">
+                Division
+                <span className="ml-2 font-bold">{player.team.division}</span>
+              </h3>
+            </div>
+          </div>
+          </a>
+          </Link>
+          
+        ))}
+      </div>
+    </div>
+  );
+}
