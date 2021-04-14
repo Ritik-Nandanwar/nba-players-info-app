09a0e13abd713ba11263a0aeb73dce89dcce1934
Ritik Nandanwar <nandanwarritik10@gmail.com>
2021-04-14 14:02:21 +0530

    t.min.css included

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 // import 'tailwindcss/tailwind.css'
 import Navbar from "../Components/Navbar";
-import "../styles/globals.css";
+// import "../styles/globals.css";
+import "../styles/t.min.css";
 import Layout from "./Layout";
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Navbar/>
-    <Component {...pageProps} />
-  </Layout>;
+  return (
+    <Layout>
+      <Navbar />
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
